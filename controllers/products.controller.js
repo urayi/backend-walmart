400d9038e1883133d9e233e846a8925ec6758c5e
José Polanco <polancoguramy@gmail.com>
2020-07-21 11:19:26 -0400

    cambios para conectar a la base de datos

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,16 +1,25 @@
-const Product = require('../models/product.model')
+const Product = require('../models/product.model');
+const { query } = require('express');
 
 getProducts = async (req, res) => {
-  await Product.find({}, (err, products) => {
-      if (err) {
-          return res.status(400).json({ success: false, error: err })
-      }
-      if (!products.length) {
-          return res
-              .status(404)
-              .json({ success: false, error: `No se encontraron Productos` })
-      }
+  console.log(req.query, req.params);
+  var query = require('url').parse(req.url, true).query;
+  await Product.find({
+    /* id: req.query !== {} ? req.query.query : null, */
+    brand: req.query !== {} ? req.query.query : null,
+    description: req.query !== {} ? req.query.query : null,
+  }, (err, products) => {
+    console.log(req.query.query);
+    if (err) {
+      return res.status(500).json({ success: false, error: err })
+    }
+    if (!products.length) {
+      return res.status(404).json({ success: false, error: `No se encontraron Productos` })
+    }
+    if (req.query.query) {
       return res.status(200).json({ success: true, data: products })
+    }
+    return res.status(200).json({ success: true, data: products })
   }).catch(err => console.log(err))
 }
 
--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose
     //.connect('mongodb://127.0.0.1:27017/database', { useNewUrlParser: true })
-    .connect('mongodb://productListUser:productListPassword@127.0.0.1:27017/promotions', { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect('mongodb://productListUser:productListPassword@127.0.0.1:27017/?authSource=admin&readPreference=primary&ssl=false', { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
