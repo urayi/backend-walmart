c0aee08c936db82e93e881cf00c490c328bb7dc8
Jose Polanco <urayi@URAYI-NITRO5.localdomain>
2020-07-26 06:46:39 -0400

    probando en duro la conexión a mongo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-require('dotenv').config({ path: 'local.env'})
+require('dotenv').config()
 const db = require('./db/db')
 const productRouter = require('./routes/products.route')
 const app = express()
